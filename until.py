6267732ed9e02075000b688df666197db354e578
mingxuan007 <44742130+mingxuan007@users.noreply.github.com>
2020-12-24 13:26:32 +0800

    Update until.py

--- until.py
+++ until.py
@@ -13,7 +13,8 @@ from sklearn.feature_extraction import stop_words
 from spacy.lang.en.stop_words import STOP_WORDS as spacy_stopwords
 from scipy.spatial.distance import cosine
 import statistics
-
+from config import load_arguments
+args=load_arguments()
 
 
 
@@ -98,8 +99,7 @@ def load_glove_model(glove_file):
             model[word] = embedding
         logger.debug("Done. {} words loaded!".format(len(model)))
     return model
-glove_path='./glove.6B.100d.txt'    
-model=load_glove_model('glove_path')
+
 
 def get_sentence_embedding(tokens, model):
     embeddings = np.asarray([model[token] for token in tokens if token in model])
@@ -180,14 +180,79 @@ def get_stopwords():
 
     return all_stopwords
 
-sp='./style-words/yelp.txt'
+
 def get_style_words():
-    with open(file=sp,
+    with open(file=args.style_words_path,
               mode='r', encoding='ISO-8859-1') as sentiment_words_file:
         words = sentiment_words_file.readlines()
     words = set(word.strip() for word in words)
     return words
 
+def get_style_transfer_score(classifier_saved_model_path, text_file, label, label_file_path):
+    with open(os.path.join(classifier_saved_model_path,
+                           vocab.txt), 'r') as json_file:
+        word_index = json.load(json_file)
+    vocab_size = len(word_index)
 
+    text_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+        num_words=global_config.vocab_size, filters=global_config.tokenizer_filters)
+    text_tokenizer.word_index = word_index
+
+
+    actual_sequences = text_tokenizer.texts_to_sequences(text_file)
+    trimmed_sequences = [
+        [x if x < vocab_size else word_index[global_config.unk_token] for x in sequence]
+        for sequence in actual_sequences]
+    text_sequences = tf.keras.preprocessing.sequence.pad_sequences(
+        trimmed_sequences, maxlen=global_config.max_sequence_length, padding='post',
+        truncating='post', value=word_index[global_config.eos_token])
+
+    x_test = np.asarray(text_sequences)
+
+    if type(y_test)!=list:
+       y_test = np.asarray([int(label)] * len(text_sequences))
+    else:
+       y_test = np.asarray(label) 
+
+    checkpoint_file = tf.train.latest_checkpoint(
+        os.path.join(classifier_saved_model_path, "checkpoints"))
+    graph = tf.Graph()
+    with graph.as_default():
+        sess = tf_session_helper.get_tensorflow_session()
+        with sess.as_default():
+            # Load the saved meta graph and restore variables
+            saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+            saver.restore(sess, checkpoint_file)
+
+            # Get the placeholders from the graph by name
+            input_x = graph.get_operation_by_name("input_x").outputs[0]
+            # input_y = graph.get_operation_by_name("input_y").outputs[0]
+            dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
+
+            # Tensors we want to evaluate
+            predictions = graph.get_operation_by_name("output/predictions").outputs[0]
+
+            # Generate batches for one epoch
+            batches = data_processor.batch_iter(list(x_test), mconf.batch_size, 1, shuffle=False)
+
+            # Collect the predictions here
+            all_predictions = []
+
+            for x_test_batch in batches:
+                batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
+                all_predictions = np.concatenate([all_predictions, batch_predictions])
+
+        sess.close()
+
+    # Print accuracy if y_test is defined
+    if y_test is not None:
+        correct_predictions = float(sum(all_predictions == y_test))
+        accuracy = correct_predictions / float(len(y_test))
+        # f1_score = metrics.f1_score(y_true=y_test, y_pred=all_predictions)
+        confusion_matrix = metrics.confusion_matrix(y_true=y_test, y_pred=all_predictions)
+        return [accuracy, confusion_matrix]
+
+    logger.info("Nothing to evaluate")
+    return [0.0, None]
 
 
