28db309bad989b7508c44d2f2eb401b9c5cfe312
mingxuan007 <44742130+mingxuan007@users.noreply.github.com>
2021-06-28 20:51:04 +0800

    Update TextCNN.py

--- classifier/TextCNN.py
+++ classifier/TextCNN.py
@@ -19,13 +19,13 @@ class TextCNN(object):
 
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            if embedding:
+#           if embedding:
 
-               self.W=embedding
-            else:
-               self.W = tf.Variable(
-                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                    name="W")
+#              self.W=embedding
+#           else:
+            self.W = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                name="W")
             self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
